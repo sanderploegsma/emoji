9883951e9ef3142319abf8e24360a29aac427f53
Sander Ploegsma <sploegsma@bol.com>
2018-07-06 17:00:55 +0200

    Generate random emoji because why not

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate rand;
+
+use std::char;
+use rand::*;
+
 fn main() {
-    println!("Hello, world!");
+    let x: u32 = thread_rng().gen_range(0x1F600, 0x1F64F);
+    let emoji = char::from_u32(x).unwrap_or('💔');
+    println!("{}", emoji);
 }
